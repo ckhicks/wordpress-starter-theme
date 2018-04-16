ce1eb4318d56735d9e95a00b99c85f7e4defdd59
CK Hicks <contact@ckhicks.com>
2018-04-16 06:06:01 -0500

    oops

---
+++ functions/enqueue-scripts.php
@@ -0,0 +1,18 @@
+<?php
+function enqueue_custom_scripts() {
+	// Add main stylesheet
+	wp_register_style('style-main', (get_stylesheet_directory_uri() . '/style.css'), false, false, 'screen');
+	wp_enqueue_style('style-main');
+	// Add print stylesheet
+	// wp_register_style('style-print', (get_stylesheet_directory_uri() . '/print.css'), false, false, 'print');
+	// wp_enqueue_style('style-print');
+	// Add modernizr to the header
+	// Remove default jQuery and add Google hosted version to the footer
+	wp_deregister_script('jquery-core');
+	wp_register_script('jquery-core', ('//ajax.googleapis.com/ajax/libs/jquery/3.1.2/jquery.min.js'), false, '3.1.2', true);
+	wp_enqueue_script('jquery-core');
+	// Load the site's main.js file but make sure jQuery is there first
+	wp_register_script('script-main', (get_stylesheet_directory_uri() . '/assets/js/main.js'), array('jquery'), '1.0.0', true);
+	wp_enqueue_script('script-main');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
